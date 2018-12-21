2df0154b25f9bd42d587ba23d537bc5399b10014
Darran Shepherd <darran.shepherd@bankbi.com>
2018-12-21 13:18:17 +0000

    Fix status property checked when looping Allow passing AcmeServer

--- Create-Certificate.ps1
+++ Create-Certificate.ps1
@@ -10,7 +10,8 @@ Param(
     $ZoneName,
     $AppService,
     $Wildcard="false",
-    $AciName="letsencrypt"
+    $AciName="letsencrypt",
+    $AcmeServer="https://acme-v02.api.letsencrypt.org/directory"
 )
 
 $resourceID=$(az identity show --resource-group $ResourceGroup --name $IdentityName --query id --output tsv)
@@ -26,13 +27,12 @@ az container create `
     --azure-file-volume-account-key $StorageKey `
     --azure-file-volume-share-name $StorageShare `
     --azure-file-volume-mount-path '/mnt/letsencrypt' `
-    --environment-variables EMAIL="$Email" DOMAIN="$Domain" WILDCARD=$Wildcard RESOURCE_GROUP=$ResourceGroup ZONE_NAME=$ZoneName APP_SERVICE=$AppService `
+    --environment-variables EMAIL="$Email" DOMAIN="$Domain" WILDCARD=$Wildcard RESOURCE_GROUP=$ResourceGroup ZONE_NAME=$ZoneName APP_SERVICE=$AppService ACME_SERVER=$AcmeServer `
     --output table
 
 do {
     Start-Sleep -Seconds 5
-    $status=$(az container show --resource-group $ResourceGroup --name $AciName --query provisioningState --output tsv)
-    Write-Host $status
+    $status=$(az container show --resource-group $ResourceGroup --name $AciName --query instanceView.state --output tsv)
 } until ($status -eq "Succeeded" -or $status -eq "Failed")
 
 az container logs --resource-group $ResourceGroup --name $AciName 
