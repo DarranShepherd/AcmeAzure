a618402428ef73a32f049d5a109191a499ce368d
Darran Shepherd <darran.shepherd@bankbi.com>
2023-05-23 15:25:17 +0100

    Use RSA instead of the default ECDSA (since certbot v2)

--- acmeazure.sh
+++ acmeazure.sh
@@ -12,16 +12,19 @@ else
     export DEPLOY_HOOK="//acmeazure//azurewebappbind.sh"
 fi
 
+certbot --version
+
 certbotargs=(
     "certonly"
     "--manual"
     "--noninteractive"
+    "--key-type" "rsa"
+    "--rsa-key-size" "2048"
     "--preferred-challenges" "dns-01"
     "--manual-auth-hook" "//acmeazure//azurednsauthenticator.sh"
     "--manual-cleanup-hook" "//acmeazure//azurednscleanup.sh"
     "--deploy-hook" $DEPLOY_HOOK
     "--agree-tos"
-    "--manual-public-ip-logging-ok"
     "--server" "$ACME_SERVER"
     "--email" "$EMAIL"
     "-d" "$DOMAIN"
